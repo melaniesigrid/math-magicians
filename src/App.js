c239b6b7d1c17070c29863634c40d208ad1f9ea2
Melanie Arellano <melaniesigrid@protonmail.com>
2022-02-09 12:29:08 -0600

    Change App to class based component, update favicon, center calculator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;

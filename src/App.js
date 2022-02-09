43d3e6bb3a733929fa51bfdf94994e0416efd283
Melanie Arellano <melaniesigrid@protonmail.com>
2022-02-09 12:32:01 -0600

    Correct linter error that prefers stateless function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Calculator from './components/Calculator';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
   render() {
     return (

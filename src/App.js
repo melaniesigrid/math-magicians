a7e52307f9c31baa87055b540d3788f7e4eec0a2
Melanie Arellano <melaniesigrid@protonmail.com>
2022-02-17 21:25:32 -0600

    Correct more style errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {
   Routes,
-  Route
+  Route,
 } from 'react-router-dom';
 import Header from './components/Header';
 import Calculator from './components/Calculator';
--- src/pages/About.js
+++ src/pages/About.js
@@ -17,14 +17,19 @@ const About = () => {
     <div>
       <h2 style={titleStyle}>Welcome to Math-Magicians!</h2>
       <p style={paragraphStyle}>
-        &quot;Math magicians&quot; is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to:
+        &quot;Math magicians&quot; is a website for all fans of mathematics.
+        <span> </span>
+        It is a Single Page App (SPA) that allows users to:
         <ul>
           <li>Make simple calculations.</li>
           <li>Read a random math-related quote.</li>
         </ul>
-        It was my first project using React. If you enjoyed it, check out my other projects on<span> </span>
-        <a href="https://github.com/melaniesigrid">
-        GitHub</a>.
+        It was my first project using React. If you enjoyed it, check out my other projects on
+        <span> </span>
+          <a href="https://github.com/melaniesigrid">
+            GitHub
+          </a>
+        .
       </p>
     </div>
   );
